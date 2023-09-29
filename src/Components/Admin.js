6ea9164ab5abdd64d3d7d0194db4a47f9a50d519
Casatti Ivan <casativan@gmail.com>
2023-09-29 17:13:01 -0300

    Css admin and home page

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -3,11 +3,14 @@ import { Link } from "react-router-dom"
 const Admin = () => {
     return (
         <section>
-            <h1>Admins Page</h1>
-            <br />
-            <p>You must have been assigned an Admin role.</p>
-            <div className="flexGrow">
-                <Link to="/">Home</Link>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Admins Page</h1>
+                <br />
+                <p className="mt-10 text-center text-xl font-bold leading-9 tracking-tight text-gray-900">Welcome to the admin page</p>
+                <div className="flexGrow">
+                    <Link className="mt-5 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        to="/">Go Home</Link>
+                </div>
             </div>
         </section>
     )
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -16,20 +16,20 @@ const Home = () => {
     return (
         <section>
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Home</h1>
-            <br />
-            <p className="mt-10 text-center text-sm text-gray-500">
-                You are logged in!<br />
-            <span className="line">
-            <Link className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-                    to="/admin">Go to the Admin page</Link>
-            </span>
-            </p>
-            <br />
-            <div className="flexGrow">
-                <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
+                <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Home</h1>
+                <br />
+                <p className="mt-10 text-center text-sm text-gray-500">
+                    You are logged in!<br />
+                    <span className="line">
+                        <Link className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                            to="/admin">Go to the Admin page</Link>
+                    </span>
+                </p>
+                <br />
+                <div className="flexGrow">
+                    <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         onClick={logout}>Sign Out</button>
-            </div>
+                </div>
             </div>
         </section>
     )
